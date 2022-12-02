2d7182f8adb80763991a9b089fbda5d87cfcbda5
SilvaVargasPablo <73146647+SilvaVargasPablo@users.noreply.github.com>
2022-12-02 01:01:35 -0300

    nombres_0

---
+++ main.js
@@ -0,0 +1,43 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        lista: [
+            {
+                nombre: "Cosme",
+                apellido: "Fulanito",
+                run: 19568235-4,
+                nacimiento: "12-04-67",
+                edad: 52
+
+            },
+
+            {
+                nombre: "John",
+                apellido: "Wick",
+                run: 12456325-8,
+                nacimiento: "09-009-86",
+                edad: 35
+
+            },
+
+            {
+                nombre: "Marinela",
+                apellido: "Volskwagen",
+                run: 25365851-8,
+                nacimiento: "26-05-00",
+                edad: 21
+
+            },
+
+            {
+                nombre: "Francesco",
+                apellido: "Virgolini",
+                run: 17685355-1,
+                nacimiento: "01-01-1976",
+                edad: 45
+
+            },
+        ]
+    }
+})
+
